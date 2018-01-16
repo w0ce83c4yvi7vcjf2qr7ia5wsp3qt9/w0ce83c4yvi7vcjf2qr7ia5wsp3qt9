01fbf5d6eaf5d5a504a87f0438fe1e1bb81239d1
WhoAmI <8114s821347+github@gmail.com>
2018-01-16 01:24:49 -0200

    Add netlify script

---
+++ netlify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# --------------------------
+
+HUGO_VERSION=0.17.1
+HUGO_BINARY=hugo_${HUGO_VERSION}_linux-64bit
+HUGO_TARBALL=${HUGO_BINARY}.tar.gz
+
+curl -LJO https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} &&
+tar -zxvf ./${HUGO_TARBALL} &&
+chmod +x ./${HUGO_BINARY}/hugo
+
+./${HUGO_BINARY}/hugo version
+
+mkdir public && echo -n 'hello 2!' > ./public/index.html
