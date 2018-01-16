5784fda90d4af3150e92b26ecda2cd734a524e5b
WhoAmI <8114s821347+github@gmail.com>
2018-01-16 10:55:40 -0200

    Update netlify shell 1

--- netlify.sh
+++ netlify.sh
@@ -7,10 +7,11 @@ HUGO_TARBALL=${HUGO_BINARY}.tar.gz
 
 # curl -sL https://github.com/amaurycatelan/hugo/releases/download/v0.17.1/hugo_0.17.1_linux-64bit.tar.gz | tar xz
 
-curl -LJO https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} &&
-tar -zxvf ./${HUGO_TARBALL} &&
+# curl -LJO https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} &&
+# tar -zxvf ./${HUGO_TARBALL} &&
+curl -sL https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} | tar xz &&
 chmod +x ./${HUGO_BINARY}/hugo
 
 ./${HUGO_BINARY}/hugo version
 
-mkdir public && echo -n 'hello 5!' > ./public/index.html
+mkdir public && echo -n 'hello 7!' > ./public/index.html
