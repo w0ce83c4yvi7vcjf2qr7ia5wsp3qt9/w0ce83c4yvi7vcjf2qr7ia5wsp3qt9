77268f2a08c6ac0dd1115ef302559567f091e9d1
WhoAmI <8114s821347+github@gmail.com>
2018-01-16 22:12:10 -0200

    Update netlify script

--- netlify.sh
+++ netlify.sh
@@ -5,13 +5,18 @@ HUGO_VERSION=0.17.1
 HUGO_BINARY=hugo_${HUGO_VERSION}_linux-64bit
 HUGO_TARBALL=${HUGO_BINARY}.tar.gz
 
+# 1
 # curl -sL https://github.com/amaurycatelan/hugo/releases/download/v0.17.1/hugo_0.17.1_linux-64bit.tar.gz | tar xz
-
 # curl -LJO https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} &&
 # tar -zxvf ./${HUGO_TARBALL} &&
-curl -sL https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} | tar xz &&
-chmod +x ./${HUGO_BINARY}/hugo
 
-./${HUGO_BINARY}/hugo version
+# 2
+# curl -sL https://github.com/amaurycatelan/hugo/releases/download/v${HUGO_VERSION}/${HUGO_TARBALL} | tar xz
+# chmod +x ./${HUGO_BINARY}/hugo
 
-mkdir public && echo -n 'hello 7!' > ./public/index.html
+# make run
+# ./${HUGO_BINARY}/hugo version
+
+ls ./node_modules/hugo-backpack/
+
+mkdir public && echo -n 'hello 8!' > ./public/index.html
